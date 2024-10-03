c687d3c3698400b7ea9b31c45b16a5c75d63ba21
Alexandr Kurganov <alexandr.kurs.it@gmail.com>
2024-10-03 15:44:13 +0600

    Add ToolBar.tsx

--- src/components/ToolBar/ToolBar.tsx
+++ src/components/ToolBar/ToolBar.tsx
@@ -1,6 +1,6 @@
 const ToolBar = () => {
   return (
-    <nav className="navbar bg-primary">
+    <nav className="navbar bg-black">
       <div className="container">
         <span className="navbar-brand mb-0 text-white fs-1">Management</span>
       </div>
